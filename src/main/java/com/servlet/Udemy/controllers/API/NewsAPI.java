a66d68c245fc15062af5ce1a630282bc50e62a50
hinerumasato <Moc Lan>
2024-01-02 20:15:15 +0700

    news

---
+++ src/main/java/com/servlet/Udemy/controllers/API/NewsAPI.java
@@ -0,0 +1,5 @@
+package com.servlet.Udemy.controllers.API;
+
+public class NewsAPI {
+    
+}
--- src/main/java/com/servlet/Udemy/controllers/admin/AddNewsController.java
+++ src/main/java/com/servlet/Udemy/controllers/admin/AddNewsController.java
@@ -1,5 +1,11 @@
 package com.servlet.Udemy.controllers.admin;
 
-public class AddNewsController {
-    
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/admin/news/add-news")
+@MultipartConfig(maxFileSize = 1024 * 1024 * 5, maxRequestSize = 1024 * 1024 * 10)
+public class AddNewsController extends HttpServlet {
+
 }
